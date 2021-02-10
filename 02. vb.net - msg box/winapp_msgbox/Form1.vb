f5dece7cd8db7059501c2cdacb15399066afb995
4bumuhammad <dhony.priniardi.m@gmail.com>
2021-02-10 23:52:40 +0700

    Latihan penggunaan messagebox

---
+++ 02. vb.net - msg box/winapp_msgbox/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("Isi Pesan", MsgBoxStyle.Information, "Judul Pesan")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim yesNo As String
+        yesNo = MsgBox("Lanjutkan?", MsgBoxStyle.YesNo, "Pesan")
+        If yesNo = DialogResult.Yes Then
+            MsgBox("Anda menekan tomnol Yes")
+        ElseIf yesNo = DialogResult.No Then
+            MsgBox("Anda menekan tombol No")
+        End If
+    End Sub
+End Class
---
+++ 02. vb.net - msg box/winapp_msgbox/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("winapp_msgbox")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("winapp_msgbox")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("022169d5-07d8-4afa-972b-6bdc275a02a9")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
