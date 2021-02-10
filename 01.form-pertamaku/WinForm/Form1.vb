2d7c8090880574a703a5847b7c8058a70320025f
4bumuhammad <dhony.priniardi.m@gmail.com>
2021-02-10 23:31:03 +0700

    Perubahan display text tahap penyempurnaan 3

--- 01.form-pertamaku/WinForm/Form1.vb
+++ 01.form-pertamaku/WinForm/Form1.vb
@@ -2,4 +2,8 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Me.TextBox1.Text = "Bismillah"
     End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Text = "Form Pertamaku"
+    End Sub
 End Class
